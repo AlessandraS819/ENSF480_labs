772b66c72e1e47ac5f0a72a8ee5ce4c6d62ce4a3
Umar Khan <muk779912@gmail.com>
2024-09-28 22:45:15 -0600

    completed: completed Lab03 ExB

--- Lab03 -- In Progress/ExB/iterator.cpp
+++ Lab03 -- In Progress/ExB/iterator.cpp
@@ -1,220 +1,133 @@
 // iterator.cpp
-// ENSF 480 - Fall 2022 - Lab 3, Ex B
+// ENSF 480 - Lab 3
+// Muhammed Umar Khan
+
 
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 #include "mystring2.h"
 
 using namespace std;
 
-
+template <typename T>
 class Vector {
-public: 
-
-  class VectIter{
-    friend class Vector;
-  private:
-    Vector *v; // points to a vector object of type T
-    int index;    // represents the subscript number of the vector's
-                  // array.
-  public:
-    VectIter(Vector& x);
- 
-    int operator++();
-    //PROMISES: increments the iterator's indes and return the 
-    //          value of the element at the index position. If
-    //          index exceeds the size of the array it will 
-    //          be set to zero. Which means it will be circulated
-    //          back to the first element of the vector.
-
-    int  operator++(int);
-    // PRIMISES: returns the value of the element at the index
-    //           position, then increments the index. If
-    //           index exceeds the size of the array it will 
-    //           be set to zero. Which means it will be circulated
-    //           back to the first element of the vector.
-
-    int  operator--();
-    // PROMISES: decrements the iterator index, and return the
-    //           the value of the element at the index. If
-    //           index is less than zero it will be set to the 
-    //           last element in the aray. Which means it will be
-    //           circulated to the last element of the vector.
-
-    int  operator--(int);
-    // PRIMISES: returns the value of the element at the index
-    //           position, then decrements the index. If
-    //           index is less than zero it will be set to the 
-    //           last element in the aray. Which means it will be
-    //           circulated to the last element of the vector.
-
-    int operator *();
-    // PRIMISES: returns the value of the element at the current 
-    //           index position.
-  };
-
-  Vector(int sz); 
-  ~Vector();
-
-  int & operator[](int i);
-  // PRIMISES: returns existing value in the ith element of 
-  //           array or sets a new value to  the ith element in
-  //           array. 
-  
-	void ascending_sort();
-  // PRIMISES: sorts the vector values in ascending order. 
-	
-private:
-  int *array;               // points to the first element of an array of T
-  int size;               // size of array
-  void swap(int&, int &); // swaps the values of two elements in array
 public:
+
+    class VectIter {
+        friend class Vector;
+    private:
+        Vector *v; // points to a vector object of type T
+        int index; // represents the subscript number of the vector's array.
+    public:
+        VectIter(Vector& x);
+
+        T operator++(); // Promises: increments the iterator's index and returns the value.
+        T operator++(int); // Promises: returns the value at index then increments.
+        T operator--(); // Promises: decrements the index and returns the value.
+        T operator--(int); // Promises: returns the value at index then decrements.
+        T operator*(); // Promises: returns the value of the element at the current index.
+    };
+
+    Vector(int sz); 
+    ~Vector();
+
+    T& operator[](int i); // Promises: returns the existing value in the ith element or sets a new value.
+
+    void ascending_sort(); // Promises: sorts the vector values in ascending order.
+
+private:
+    T *array; // points to the first element of an array of T
+    int size; // size of array
+    void swap(T&, T&); // swaps the values of two elements in array
 };
 
-
-void Vector::ascending_sort()
-{
-	for(int i=0; i< size-1; i++)
-		for(int j=i+1; j < size; j++)
-			if(array[i] > array[j])
-				swap(array[i], array[j]);
+// Define swap function
+template <typename T>
+void Vector<T>::swap(T& a, T& b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
 }
 
-void Vector::swap(int& a, int& b)
-{
-	int tmp = a;
-	a = b;
-	b = tmp;
+// Define ascending_sort function
+template <typename T>
+void Vector<T>::ascending_sort() {
+    for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j < size; j++)
+            if (array[i] > array[j])
+                swap(array[i], array[j]);
 }
 
-int Vector::VectIter::operator *()
-{
-  return v -> array[index];
+// Define VectIter constructor
+template <typename T>
+Vector<T>::VectIter::VectIter(Vector& x) {
+    v = &x;
+    index = 0;
 }
 
-
-Vector::VectIter::VectIter(Vector& x)
-{
-  v = &x;
-  index = 0;
+// Define operator* for iterator
+template <typename T>
+T Vector<T>::VectIter::operator*() {
+    return v->array[index];
 }
 
-
-
-Vector::Vector(int sz)
-{
-  size=sz;
-  array = new int [sz];
-  assert (array != NULL);
+// Define operator++ for iterator (prefix)
+template <typename T>
+T Vector<T>::VectIter::operator++() {
+    if (index >= v->size) {
+        index = 0; // wrap around
+    }
+    return v->array[index++];
 }
 
-
-Vector::~Vector()
-{
-  delete [] array;
-  array = NULL;
+// Define operator++ for iterator (postfix)
+template <typename T>
+T Vector<T>::VectIter::operator++(int) {
+    T temp = v->array[index];
+    index++;
+    if (index >= v->size) {
+        index = 0; // wrap around
+    }
+    return temp;
 }
 
-
-
-int & Vector ::operator [] (int i)
-{
-  return array[i];
+// Define operator-- for iterator (prefix)
+template <typename T>
+T Vector<T>::VectIter::operator--() {
+    if (index < 0) {
+        index = v->size - 1; // wrap around to last element
+    }
+    return v->array[index--];
 }
 
-
-int main()
-{
-
- Vector x(3);
- x[0] = 999;
- x[1] = -77;
- x[2] = 88;
-
- Vector::VectIter iter(x);
-
- cout << "\n\nThe first element of vector x contains: " << *iter; 
-
- // the code between the  #if 0 and #endif is ignored by
- // compiler. If you change it to #if 1, it will be compiled
- 
-#if 0
-	cout << "\nTesting an <int> Vector: " << endl;
-	
-	cout << "\n\nTesting sort";
-	x.ascending_sort();
-	
-	for (int i=0; i<3 ; i++)
-		cout << endl << iter++;
-	
-	cout << "\n\nTesting Prefix --:";
-	for (int i=0; i<3 ; i++)
-		cout << endl << --iter;
-	
-	cout << "\n\nTesting Prefix ++:";
-	for (int i=0; i<3 ; i++)
-		cout << endl << ++iter;	
-	
-	cout << "\n\nTesting Postfix --";
-	for (int i=0; i<3 ; i++)
-		cout << endl << iter--;
-	
-	cout << endl;
-	
-	cout << "Testing a <Mystring> Vector: " << endl;
-	Vector<Mystring> y(3);
-	y[0] = "Bar";
-	y[1] = "Foo";
-	y[2] = "All";;
-	
-	Vector<Mystring>::VectIter iters(y);
-	
-	cout << "\n\nTesting sort";
-	y.ascending_sort();
-	
-	for (int i=0; i<3 ; i++)
-		cout << endl << iters++;
-	
-	cout << "\n\nTesting Prefix --:";
-	for (int i=0; i<3 ; i++)
-		cout << endl << --iters;
-	
-	cout << "\n\nTesting Prefix ++:";
-	for (int i=0; i<3 ; i++)
-		cout << endl << ++iters;
-	
-	cout << "\n\nTesting Postfix --";
-	for (int i=0; i<3 ; i++)
-		cout << endl << iters--;
-	
-	cout << endl; cout << "Testing a <char *> Vector: " << endl;
-	Vector<const char*> z(3);
-	z[0] = "Orange";
-	z[1] = "Pear";
-	z[2] = "Apple";;
-	
-	Vector<const char*>::VectIter iterchar(z);
-	
-	cout << "\n\nTesting sort";
-	z.ascending_sort();
-	
-	for (int i=0; i<3 ; i++)
-		cout << endl << iterchar++;
-	
-#endif
-	cout << "\nPrgram Terminated Successfully." << endl;
-	
-	return 0;
+// Define operator-- for iterator (postfix)
+template <typename T>
+T Vector<T>::VectIter::operator--(int) {
+    T temp = v->array[index];
+    index--;
+    if (index < 0) {
+        index = v->size - 1; // wrap around to last element
+    }
+    return temp;
 }
 
+// Define Vector constructor
+template <typename T>
+Vector<T>::Vector(int sz) {
+    size = sz;
+    array = new T[sz];
+    assert(array != NULL);
+}
 
+// Define Vector destructor
+template <typename T>
+Vector<T>::~Vector() {
+    delete[] array;
+    array = NULL;
+}
 
-
-
-
-
-
-
-
-
-
+// Define operator[] for Vector
+template <typename T>
+T& Vector<T>::operator[](int i) {
+    return array[i];
+}
