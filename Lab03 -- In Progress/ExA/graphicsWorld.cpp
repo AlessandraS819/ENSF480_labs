6c7043bdb709365a2709e720214d6350df191932
Umar Khan <muk779912@gmail.com>
2024-09-29 22:05:45 -0600

    fix: updating graphicsWorld to add the test code for Lab03 ExA

--- Lab03 -- In Progress/ExA/graphicsWorld.cpp
+++ Lab03 -- In Progress/ExA/graphicsWorld.cpp
@@ -13,24 +13,24 @@
 #include <iostream>
 
 void GraphicsWorld::run() {
-    std::cout << "Program by Muhammed Umar Khan" << std::endl;
-
-    // Test for Point class:
+#if 0 // Change 0 to 1 to test Point
     Point m(6, 8);
     Point n(6, 8);
-    n.setX(9);
-    std::cout << "\nExpected to display the distance between m and n is: 3";
-    std::cout << "\nThe distance between m and n is: " << m.distance(n);
-    std::cout << "\nExpected second version of the distance function also print: 3";
-    std::cout << "\nThe distance between m and n is again: " << Point::distance(m, n);
-    
-    // Test for Square class:
-    std::cout << "\n\nTesting Functions in class Square:" << std::endl;
+    n.setx(9);
+    cout << "\nExpected to display the distance between m and n is: 3";
+    cout << "\nThe distance between m and n is: " << m.distance(n);
+    cout << "\nExpected second version of the distance function also print: 3";
+    cout << "\nThe distance between m and n is again: " << Point::distance(m, n);
+    #endif // end of block to test Point
+
+#if 0 // Change 0 to 1 to test Square
+    cout << "\n\nTesting Functions in class Square:" << endl;
     Square s(5, 7, 12, "SQUARE - S");
     s.display();
+    #endif // end of block to test Square
 
-    // Test for Rectangle class:
-    std::cout << "\nTesting Functions in class Rectangle:" << std::endl;
+#if 0 // Change 0 to 1 to test Rectangle
+    cout << "\nTesting Functions in class Rectangle:";
     Rectangle a(5, 7, 12, 15, "RECTANGLE A");
     a.display();
 
@@ -38,54 +38,114 @@ void GraphicsWorld::run() {
     b.display();
 
     double d = a.distance(b);
-    std::cout << "\nDistance between rectangle a, and b is: " << d << std::endl;
+    cout << "\nDistance between rectangle a, and b is: " << d << endl;
 
-    // Test copy constructor and assignment operator
-    Rectangle rec1 = a; // copy constructor
+    Rectangle rec1 = a;
     rec1.display();
 
-    std::cout << "\nTesting assignment operator in class Rectangle:" << std::endl;
+    cout << "\nTesting assignment operator in class Rectangle:" << endl;
     Rectangle rec2(3, 4, 11, 7, "RECTANGLE rec2");
     rec2.display();
-    rec2 = a; // assignment operator
+    rec2 = a;
 
     a.set_side_b(200);
     a.set_side_a(100);
-    std::cout << "\nExpected to display the following values for object rec2:" << std::endl;
-    std::cout << "Rectangle Name: RECTANGLE A\n" 
-              << "X-coordinate: 5\n"
-              << "Y-coordinate: 7\n"
-              << "Side a: 12\n"
-              << "Side b: 15\n"
-              << "Area: 180\n"
-              << "Perimeter: 54\n";
+
+    cout << "\nExpected to display the following values for object rec2: " << endl;
+    cout << "Rectangle Name: RECTANGLE A\n"
+         << "X-coordinate: 5\n"
+         << "Y-coordinate: 7\n"
+         << "Side a: 12\n"
+         << "Side b: 15\n"
+         << "Area: 180\n"
+         << "Perimeter: 54\n";
+
+    cout << "\nIf it doesn't, there is a problem with your assignment operator.\n" << endl;
     rec2.display();
 
-    std::cout << "\nTesting copy constructor in class Rectangle:" << std::endl;
-    Rectangle rec3(a);  // copy constructor
+    cout << "\nTesting copy constructor in class Rectangle:" << endl;
+    Rectangle rec3(a);
     rec3.display();
 
     a.set_side_b(300);
     a.set_side_a(400);
-    std::cout << "\nExpected to display the following values for object rec3:" << std::endl;
-    std::cout << "Rectangle Name: RECTANGLE A\n" 
-              << "X-coordinate: 5\n"
-              << "Y-coordinate: 7\n"
-              << "Side a: 100\n"
-              << "Side b: 200\n"
-              << "Area: 20000\n"
-              << "Perimeter: 600\n";
-    rec3.display();
 
-    // Test for polymorphism:
-    std::cout << "\nTesting array of pointers and polymorphism:" << std::endl;
+    cout << "\nExpected to display the following values for object rec3: " << endl;
+    cout << "Rectangle Name: RECTANGLE A\n"
+         << "X-coordinate: 5\n"
+         << "Y-coordinate: 7\n"
+         << "Side a: 100\n"
+         << "Side b: 200\n"
+         << "Area: 20000\n"
+         << "Perimeter: 600\n";
+
+    cout << "\nIf it doesn't, there is a problem with your assignment operator.\n" << endl;
+    rec3.display();
+    #endif // end of block to test Rectangle
+
+#if 0 // Change 0 to 1 to test using array of pointers and polymorphism
+    cout << "\nTesting array of pointers and polymorphism:" << endl;
     Shape* sh[4];
     sh[0] = &s;
     sh[1] = &b;
     sh[2] = &rec1;
     sh[3] = &rec3;
 
-    for (int i = 0; i < 4; ++i) {
-        sh[i]->display();  // Polymorphism in action
-    }
-}
+    sh[0]->display();
+    sh[1]->display();
+    sh[2]->display();
+    sh[3]->display();
+    #endif // end of block to test array of pointers and polymorphism
+
+#if 0
+    cout << "\nTesting Functions in class Circle:" << endl;
+    Circle c(3, 5, 9, "CIRCLE C");
+    c.display();
+    cout << "The area of " << c.getName() << " is: " << c.area() << endl;
+    cout << "The perimeter of " << c.getName() << " is: " << c.perimeter() << endl;
+
+    d = a.distance(c);
+    cout << "\nThe distance between rectangle a and circle c is: " << d;
+
+    CurveCut rc(6, 5, 10, 12, 9, "CurveCut rc");
+    rc.display();
+    cout << "The area of " << rc.getName() << " is: " << rc.area() << endl;
+    cout << "The perimeter of " << rc.getName() << " is: " << rc.perimeter() << endl;
+
+    d = rc.distance(c);
+    cout << "\nThe distance between rc and c is: " << d;
+
+    // Using array of Shape pointers:
+    Shape* sh[4];
+    sh[0] = &s;
+    sh[1] = &a;
+    sh[2] = &c;
+    sh[3] = &rc;
+
+    sh[0]->display();
+    cout << "\nThe area of " << sh[0]->getName() << " is: " << sh[0]->area() << endl;
+    cout << "The perimeter of " << sh[0]->getName() << " is: " << sh[0]->perimeter() << endl;
+
+    sh[1]->display();
+    cout << "\nThe area of " << sh[1]->getName() << " is: " << sh[1]->area() << endl;
+    cout << "The perimeter of " << sh[1]->getName() << " is: " << sh[1]->perimeter() << endl;
+
+    sh[2]->display();
+    cout << "\nThe area of " << sh[2]->getName() << " is: " << sh[2]->area() << endl;
+    cout << "The circumference of " << sh[2]->getName() << " is: " << sh[2]->perimeter() << endl;
+
+    sh[3]->display();
+    cout << "\nThe area of " << sh[3]->getName() << " is: " << sh[3]->area() << endl;
+    cout << "The perimeter of " << sh[3]->getName() << " is: " << sh[3]->perimeter() << endl;
+
+    cout << "\nTesting copy constructor in class CurveCut:" << endl;
+    CurveCut cc = rc;
+    cc.display();
+
+    cout << "\nTesting assignment operator in class CurveCut:" << endl;
+    CurveCut cc2(2, 5, 100, 12, 9, "CurveCut cc2");
+    cc2.display();
+    cc2 = cc;
+    cc2.display();
+#endif
+} // END OF FUNCTION run
