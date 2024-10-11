4773f6b63dfdd2c0cd9ceb2a11c91082f4ff113f
Umar Khan <muk779912@gmail.com>
2024-10-11 11:42:32 -0600

    update: updating lab04 exC

---
+++ Lab04 -- In Progress/ExC/car.cpp
@@ -0,0 +1,12 @@
+// Filename: car.cpp
+// Authors: Alessandra Schiavi and Muhammed Umar Khan
+// Lab: Lab 04 Ex C
+
+class Car extends Vehicle{
+    public:
+        void turn(){
+            //Turning Logic
+        }
+    private:
+        int seats;
+}ß
---
+++ Lab04 -- In Progress/ExC/moveable.cpp
@@ -0,0 +1,10 @@
+// Filename: moveable.cpp
+// Authors: Alessandra Schiavi and Muhammed Umar Khan
+// Lab: Lab 04 Ex C
+
+interface Movable{
+    public:
+        void forward(){};
+
+        void backward(){};
+}
