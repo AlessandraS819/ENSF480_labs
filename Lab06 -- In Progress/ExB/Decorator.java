7884f8c7c680ed280a023620ab8ccb1a22e7c3e4
Umar Khan <muk779912@gmail.com>
2024-11-03 14:44:53 -0700

    fix: reordering and assisgning nessecasry files

---
+++ Lab06 -- In Progress/ExB/Decorator.java
@@ -0,0 +1,8 @@
+public abstract class Decorator implements Component{
+    private Component cmp;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+}
---
+++ Lab06 -- In Progress/ExB/Text.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+
+public class Text implements Component{
+    private int x;
+    private int y;
+    private String text;
+
+    @Override
+    public void draw (Graphics g){
+
+    }
+
+    
+}
