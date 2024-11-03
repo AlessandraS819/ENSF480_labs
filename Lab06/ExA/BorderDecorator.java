e7040f7fd3a1fe40fc07871f1307ab97eeb5f7ae
Alessandra Schiavi Paz <alessandra.schiavipa@ucalgary.ca>
2024-11-02 21:35:40 -0600

    exA

---
+++ Lab06/ExA/BorderDecorator.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics;
+
+public class BorderDecorator extends Decorator {
+    
+    @Override
+    public void draw(Graphics g){
+
+    }
+}
---
+++ Lab06/ExA/ColourFrameDecorator.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics;
+
+public class ColourFrameDecorator extends Decorator{
+   
+    @Override
+    public void draw(Graphics g){
+
+    }
+}
